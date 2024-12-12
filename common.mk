31efbd7b75db41371a45be6dd39d80124ff9cdc2
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    sm6125-common: Build DSPVolumeSynchronizer
    Change-Id: I46f37b21fc9f0dd7cfb7f1d72ebd07ae5375bb2f

--- common.mk
+++ common.mk
@@ -183,6 +183,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
